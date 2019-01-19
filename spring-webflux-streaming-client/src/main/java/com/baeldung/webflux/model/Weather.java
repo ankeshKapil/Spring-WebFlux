05d004c01ef0d5d2be7e54e47e46c05cbc890ddc
ankeshkapil <ankesh.kapil@gmail.com>
2019-01-19 10:38:54 +0530

    Pushing to git

---
+++ spring-webflux-streaming-client/src/main/java/com/baeldung/webflux/model/Weather.java
@@ -0,0 +1,16 @@
+package com.baeldung.webflux.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+public class Weather {
+
+    private String temprature;
+    private String humidity;
+    private String windSpeed;
+
+}
